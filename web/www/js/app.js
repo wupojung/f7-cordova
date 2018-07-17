26cd02a4ebef0a693dfc295a1bdc54f93664b409
pojung.wu <pojung.wu@wowprime.com>
2018-07-17 15:01:24 +0800

    合併訂位架構

--- web/www/js/app.js
+++ web/www/js/app.js
@@ -4,29 +4,38 @@ var $ = Dom7;
 // Theme
 var theme = 'auto';
 if (document.location.search.indexOf('theme=') >= 0) {
-  theme = document.location.search.split('theme=')[1].split('&')[0];
+    theme = document.location.search.split('theme=')[1].split('&')[0];
 }
 
 // Init App
 var app = new Framework7({
-  id: 'io.framework7.testapp',
-  root: '#app',
-  theme: theme,
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-    };
-  },
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
+    id: 'io.framework7.testapp',
+    root: '#app',
+    theme: theme,
+    data: function () {
+        return {
+            user: {
+                firstName: 'John',
+                lastName: 'Doe',
+            },
+        };
     },
-  },
-  routes: routes,
-  vi: {
-    placementId: 'pltd4o7ibb9rc653x14',
-  },
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    routes: routes,
+    // Enable panel left visibility breakpoint
+    panel: {
+        leftBreakpoint: 960,
+    },
+    vi: {
+        placementId: 'pltd4o7ibb9rc653x14',
+    },
+});
+
+// Init/Create main view
+var mainView = app.views.create('.view-main', {
+    url: '/'
 });
