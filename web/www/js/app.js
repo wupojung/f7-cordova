00faabca76fe2fde5ca3ea17f0f7f2471428d745
pojung.wu <pojung.wu@wowprime.com>
2018-07-19 17:25:25 +0800

    測試 動態list

--- web/www/js/app.js
+++ web/www/js/app.js
@@ -51,14 +51,56 @@ var $$ = app.$;
 //參考文獻:  http://framework7.io/docs/page.html
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
-    //console.log(e);
+    console.log(e);
     //console.log(e.detail.name);
-    switch (e.detail.name.toLowerCase()){
+    switch (e.detail.name.toLowerCase()) {
         case "today":
+            self.processToday();
             console.log("今天");
             break;
         case "addresv":
             console.log("訂位");
             break;
-    };
+    }
+    ;
 });
+
+function processToday() {
+    // Dummy items array
+    var items = [];
+    for (var i = 1; i <= 10000; i++) {
+        items.push({
+            title: 'Item ' + i,
+            subtitle: 'Subtitle ' + i
+        });
+    }
+    var virtualList = app.virtualList.create({
+        // List Element
+        el: '.virtual-list',
+        // Pass array with items
+        items: items,
+        // Custom search function for searchbar
+        searchAll: function (query, items) {
+            var found = [];
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].title.toLowerCase().indexOf(query.toLowerCase()) >= 0 || query.trim() === '') found.push(i);
+            }
+            return found; //return array with mathced indexes
+        },
+        // List item Template7 template
+        itemTemplate:
+        '<li>' +
+        '<a href="#" class="item-link item-content">' +
+        '<div class="item-inner">' +
+        '<div class="item-title-row">' +
+        '<div class="item-title">{{title}}</div>' +
+        '</div>' +
+        '<div class="item-subtitle">{{subtitle}}</div>' +
+        '</div>' +
+        '</a>' +
+        '</li>',
+        // Item height
+        height: app.theme === 'ios' ? 63 : 73,
+    });
+
+}
